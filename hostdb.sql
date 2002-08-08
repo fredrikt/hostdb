ae7b1ff2d15d0a537655479f2059d7efb2fdef12
Fredrik Thulin <ft@it.su.se>
2002-08-08 12:08:20 +0000

    init
    git-svn-id: svn://anonsvn.it.su.se/svn/HOSTDB/trunk@6 36f62460-150b-0410-929b-89381cca7e77

---
+++ hostdb.sql
@@ -0,0 +1,25 @@
+# $Id$
+#
+# The tables associated with hosts and DNS zone generation
+#
+
+CREATE TABLE config (
+        id INT AUTO_INCREMENT NOT NULL,
+        mac CHAR(17),
+        hostname VARCHAR(255) NOT NULL,
+        ip CHAR(15) NOT NULL,
+        owner VARCHAR(255) NOT NULL,
+        user VARCHAR(255),
+        partof INT,
+        reverse ENUM('Y', 'N') DEFAULT 'N',
+        client_id VARCHAR(255),
+        options MEDIUMBLOB,
+        PRIMARY KEY (id)
+);
+
+CREATE TABLE zone (
+        name VARCHAR(255) NOT NULL,
+        serial INT NOT NULL,
+        owner VARCHAR(255),
+        PRIMARY KEY (name)
+);
