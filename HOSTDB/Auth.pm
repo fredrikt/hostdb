9d84b650c64ed1183a585ef371c5bd885cda36d6
Fredrik Thulin <ft@it.su.se>
2003-01-14 20:46:31 +0000

    bugfixes
    git-svn-id: svn://anonsvn.it.su.se/svn/HOSTDB/trunk@238 36f62460-150b-0410-929b-89381cca7e77

--- HOSTDB/Auth.pm
+++ HOSTDB/Auth.pm
@@ -27,6 +27,7 @@ HOSTDB::Auth - Access control functions.
   	die ("You do not have write permissions to subnet '" . $subnet->subnet () . "'\n");
   }
 
+
 =head1 DESCRIPTION
 
 Database access routines.
@@ -78,19 +79,24 @@ sub is_allowed_write
 
 	$self->_debug_print ("Checking if '$candidate' may write to object '$o'");
 
+	if (defined ($self->{authorization}) and $self->{authorization} eq 'DISABLED') {
+		$self->_debug_print ("Authorization DISABLED (through configuration)");
+		return 1;
+	}
+
 	my $owner = $o->owner ();
 
 	$self->_debug_print ("Owner of object is '$owner'");
 
-	return undef unless ($owner);
+	$self->_debug_print ("Object has no owner, returning undef"), return undef unless ($owner);
 
-	return 1 if $self->_is_in_list ($candidate, "admin", $self->_admin_list ());
+	return 1 if $self->_is_in_list ($candidate, "admin", $self->admin_list ());
 
 	return 1 if $self->_is_in_list ($candidate, "object owner", split (',', $owner));
 
 	# no exact match, now go chase users in LDAP
 	
-	return 1 if $self->_is_in_list ($candidate, "LDAP admin", $self->_ldap_explode ($self->_admin_list ()));
+	return 1 if $self->_is_in_list ($candidate, "LDAP admin", $self->_ldap_explode ($self->admin_list ()));
 
 	return 1 if $self->_is_in_list ($candidate, "LDAP object owner", $self->_ldap_explode (split (',', $owner)));
 
@@ -122,6 +128,26 @@ sub ldap_server
 }
 
 
+=head1 admin_list
+
+	A list of users with rights to do whatever they please.
+
+=cut
+sub admin_list
+{
+	my $self = shift;
+
+	if (@_) {
+		my @newvalue = @_;
+
+		$self->{admins} = \@newvalue;
+		return 1;
+	}
+
+	return (wantarray ? @{$self->{admins}} : join (",", @{$self->{admins}}));
+}
+
+
 
 #####################
 # PRIVATE FUNCTIONS #
@@ -134,33 +160,12 @@ sub ldap_server
 	but are documented here as well just for the sake of documentation.
 
 
-=head1 _admin_list
-
-	A list of users with rights to do whatever they please.
-
-=cut
-sub _admin_list
-{
-	my $self = shift;
-
-	if (@_) {
-		my @newvalue = @_;
-
-		$self->{admins} = sort @newvalue;
-	
-		return 1;
-	}
-
-	return (wantarray ? $self->{admins} : join (",", $self->{admins}));
-}
-
-
 =head1 _is_in_list
 
 	Check if first argument matches exactly an item in the following list.
 
 	@l = ('foo', 'ft', 'bar');
-	if ($self->_is_in_list ('ft', "test", @list)) {
+	if ($self->_is_in_list ('ft', "test-list", @list)) {
 		print ("Yes, it was.\n");
 	}
 
@@ -171,6 +176,9 @@ sub _is_in_list
 	my $q = shift;
 	my $listname = shift;
 	my @l = @_;
+
+	return 0 unless defined ($q);
+	return 0 unless (defined ($l[0]));
 
 	if (grep (/^$q$/, @l)) {
 		$self->_debug_print ("Token '$q' found in '$listname' list");
@@ -183,25 +191,41 @@ sub _is_in_list
 }
 
 
+=head1 _ldap_explode
+
+	Takes an array of <mumble> and looks them up in LDAP. First as uid's and
+	then as cn's. Returns all uid's the LDAP search yielded.
+
+	$self->_ldap_search ('ft', 'it-sysadm');
+	
+	The above would return 'ft' (a uid) and all members of the group 'it-sysadm'.
+
+
+=cut
 sub _ldap_explode
 {
 	my $self = shift;
 	my @in = @_;
 	my @out;
+
+	return undef unless (@in);
 	
 	$self->_debug_print ("LDAP explode @in");
+	
+	$self->_set_error ('');
+
 	my $ldap = $self->{ldap};
 	if (! defined ($ldap)) {
 		my $ldap_server = $self->ldap_server ();	
 	
 		if (! defined ($ldap_server)) {
-			$self->_set_error ("No LDAP server defined\n");
+			$self->_debug_print ("No LDAP server defined, skipping LDAP search\n");
 			return undef;
 		}
 			
 		$ldap = Net::LDAP->new ($ldap_server);
 		if (! defined ($ldap)) {
-			$self->_set_error ("Could not connect to LDAP server '$ldap_server'");
+			$self->_set_error ("Could not connect to LDAP server '$ldap_server', skipping LDAP search");
 			return undef;
 		}
 		$self->{ldap} = $ldap;
@@ -238,12 +262,13 @@ sub _ldap_explode
 			}
 		}
 
-		my @t = sort keys %uid;
+		@t = sort keys %uid;
 		push (@out, @t);
 
 		$self->_debug_print ("LDAP exploding of '$token' resulted in '@t'");
 	}
 
+	#warn ("LDAP explode @in RESULT @out\n");
 	return @out;	                
 }
 
